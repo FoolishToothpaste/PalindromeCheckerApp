ee5a9cc5de251d89255ebdbe46f206a3b9258ab4
FoolishToothpaste <sohamroy947@gmail.com>
2026-03-09 14:22:15 +0530

    UseCase1 Program

--- PalindromeCheckerApp/src/appcode/PalindromeCheckerAppUseCase1.java
+++ PalindromeCheckerApp/src/appcode/PalindromeCheckerAppUseCase1.java
@@ -3,7 +3,7 @@ package appcode;
 public class PalindromeCheckerAppUseCase1 {
     public static void main(String[] args) {
         System.out.println("Welcome to the Palindrome Checker Management System Version : 1.0");
-        System.out.println("System Initialised successfully");
+        System.out.println("System Initialised successfully!");
     }
 }
 
